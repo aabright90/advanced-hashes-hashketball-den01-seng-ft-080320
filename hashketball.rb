63297663d3daa79aff672ea5fc963419c19f8594
Austen Bright <pureaustenbright@gmail.com>
2020-04-08 01:01:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -39,17 +39,6 @@ def game_hash
       :slam_dunks => 15
       },
       {
-      :player_name => "Brook Lopez",
-      :number => 11,
-      :shoe => 17,
-      :points => 17,
-      :rebounds => 19,
-      :assists => 10,
-      :steals => 3,
-      :blocks => 1,
-      :slam_dunks => 15
-      },
-      {
       :player_name => "Mason Plumlee",
       :number => 1,
       :shoe => 19,
