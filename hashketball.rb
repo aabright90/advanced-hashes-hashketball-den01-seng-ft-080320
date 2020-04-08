6dc76c3a764fc8ee59f5edb4fce6ec793b6ebab6
Austen Bright <pureaustenbright@gmail.com>
2020-04-08 01:37:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -201,5 +201,9 @@ def player_stats(name)
 end
 
 def big_shoe_rebounds
-  
+  game_hash.each do |location, team_data|
+    team_data[:players].each do |player|
+      binding.pry
+    end 
+  end 
 end
